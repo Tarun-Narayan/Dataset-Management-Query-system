fb9a5c6a110a905dbbd9d3ecd941e10134c11929
Tarun Narayan <tarun.n2002@gmail.com>
2024-10-28 19:14:50 -0700

    done

--- src/controller/RoomsAddHelpers.ts
+++ src/controller/RoomsAddHelpers.ts
@@ -3,16 +3,9 @@ import { InsightError } from "./IInsightFacade";
 import * as parse5 from "parse5";
 import http from "http";
 
-// Function to get the root folder name from the zip file
-function getRootFolderName(zipContent: JSZip): string {
-	const firstFileName = Object.keys(zipContent.files)[0];
-	return firstFileName.split("/")[0];
-}
-
 // Function to parse rooms dataset
 export async function parseRoomsZipFile(zipContent: JSZip): Promise<Map<string, any>> {
 	const fileMap = new Map<string, any>();
-	const rootFolder = getRootFolderName(zipContent);
 	const indexFilePath = "index.htm";
 	const indexFile = zipContent.files[indexFilePath];
 
